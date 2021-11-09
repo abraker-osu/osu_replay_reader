e5f9cdbdd63d2bc9b91fe9e7e264a9736b0a533f
abraker <abraker95@gmail.com>
2021-11-08 22:02:28 -0500

    import Mod

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .replay_reader.replayIO import ReplayIO
 from .replay_reader.replayIO import ReplayException
 from .replay_reader.replay_idx import ReplayIdx
+from .replay_reader.mod import Mod
