0bbde320b6c369c41cfedc541de79427d686ad8b
abraker95 <abraker95@gmail.com>
2023-07-22 18:48:18 -0400

    add stuff to imports

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .replay_reader.replayIO import ReplayIO
 from .replay_reader.replayIO import ReplayException
 from .replay_reader.replay_idx import ReplayIdx
+from .replay_reader.replay import Replay
 from .replay_reader.mod import Mod
