e8daeffdf9ca492bea4424bc27d660002ab2dd8d
abraker95 <abraker95@gmail.com>
2024-12-15 14:40:53 -0500

    mod is now part of osu_interfaces; fix package name in .gitignore

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,8 @@
-from .mod import Mod
 from .replayIO import ReplayIO, ReplayException
 from .replay import Replay
 from .replay_idx import ReplayIdx
 
 __all__ = [
-    'Mod',
     'ReplayIO',
     'ReplayException',
     'Replay',
